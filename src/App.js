05dcfc2ea0c5da7f5ec862201196f4390ea7e32f
wangle <wangle@nibirutech.com>
2018-10-23 15:36:38 +0800

    lihui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types'
 
+class Title extends Component{
+    static contextTypes={
+        themeColor:PropTypes.string,
+        handleChangeThemeColor:PropTypes.func
+    }
+    render(){
+        const {themeColor}=this.state
+        return(
+            <div style={{color:themeColor}}>
+                标题
+            </div>
+        )
+    }
+}
+class Button extends Component{
+    static contextTypes={
+        title:PropTypes.string,
+        handleChangeThemeColor:PropTypes.func
+    }
+    render(){
+        const {themeColor,handleChangeThemeColor}=this.state
+        return(
+            <div>
+               <button style={{color:themeColor}} onClick={()=>handleChangeThemeColor('red')}>red</button>
+                <button style={{color:themeColor}} onClick={()=>handleChangeThemeColor('green')}>green</button>
+            </div>
+        )
+    }
+}
 class App extends Component {
+    static childContextTypes={
+        themeColor:PropTypes.string,
+        handleChangeThemeColor:PropTypes.func
+    }
+    constructor(props){
+        super(props)
+        this.state={
+            themeColor:'red'
+        }
+    }
+    handleChangeThemeColor(color){
+        this.setState({
+        themeColor:color
+        })
+    }
+    
+    getChildContext(){
+        return {
+            themeColor:this.state.themeColor,
+            handleChangeThemeColor:(color)=>this.handleChangeThemeColor(color)
+        }
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+       <Title />
+          <Button/>
       </div>
     );
   }
